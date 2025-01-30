1be514f0a07ed7fbc971705f0776911b3882bb34
wajuejinajia <wajuejinajia@gmail.com>
2025-01-31 00:54:29 +0800

    feat: element-plus icon的引入

--- src/main.js
+++ src/main.js
@@ -5,7 +5,13 @@ import router from './router'
 // element-plus 的完整引入，需要考虑最后打包的体积
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
+// element-plus icon的引入
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 
 const app = createApp(App)
+
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+    app.component(key, component)
+}
 
 app.use(router).use(ElementPlus).mount('#app')

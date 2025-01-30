fe22323da0e8e64fd58dc7848040c4990ffe87ab
wajuejinajia <wajuejinajia@gmail.com>
2025-01-31 00:24:19 +0800

    feat: 完成路由搭建

---
+++ src/router/index.js
@@ -0,0 +1,20 @@
+import { createWebHashHistory, createRouter } from 'vue-router'
+
+
+// 制定路由规则
+
+const routes = [
+    {
+        path: '/',
+        name: 'main',
+        component: () => import("@/views/Main.vue")
+    }
+]
+
+const router = createRouter({
+    // 设置路由模式
+    history: createWebHashHistory(),
+    routes,
+})
+
+export default router
--- src/views/Main.vue
+++ src/views/Main.vue
@@ -1,5 +1,5 @@
 <template>
-  <router-view></router-view>
+    <div>i am main</div>
 </template>
 
 <script setup>

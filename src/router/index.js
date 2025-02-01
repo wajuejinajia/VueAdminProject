ab63334d97a0969729cf01500c3abcb72df2a0e3
wajuejinajia <wajuejinajia@gmail.com>
2025-02-01 23:59:34 +0800

    feat: 用户表格数据的渲染

--- src/router/index.js
+++ src/router/index.js
@@ -14,6 +14,11 @@ const routes = [
                 path: 'home',
                 name: 'home',
                 component: () => import("@/views/Home.vue"),
+            },
+            {
+                path: 'user',
+                name: 'user',
+                component: () => import("@/views/User.vue"),
             }
         ]
     }

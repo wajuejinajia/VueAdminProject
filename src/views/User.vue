4ab1ca94f1f527fe112eba6de7abf8511371688c
wajuejinajia <wajuejinajia@gmail.com>
2025-02-02 16:59:05 +0800

    feat: 用户编辑组件的实现

--- src/views/User.vue
+++ src/views/User.vue
@@ -34,6 +34,65 @@
         :total="config.total"
         @current-change="handleChange"/>
     </div>
+
+    <el-dialog
+    v-model="dialogVisible"
+    :title="action == 'add' ? '新增用户' : '编辑用户'"
+    width="35%"
+    :before-close="handleClose"
+  >
+       <!--需要注意的是设置了:inline="true"，
+		会对el-select的样式造成影响，我们通过给他设置一个class=select-clearn
+		在css进行处理-->
+    <el-form :inline="true"  :model="formUser" :rules="rules" ref="userForm">
+      <el-row>
+        <el-col :span="12">
+          <el-form-item label="姓名" prop="name">
+            <el-input v-model="formUser.name" placeholder="请输入姓名" />
+          </el-form-item>
+        </el-col>
+        <el-col :span="12">
+          <el-form-item label="年龄" prop="age">
+            <el-input v-model.number="formUser.age" placeholder="请输入年龄" />
+          </el-form-item>
+        </el-col>
+      </el-row>
+      <el-row>
+        <el-col :span="12">
+          <el-form-item class="select-clearn" label="性别" prop="sex">
+            <el-select  v-model="formUser.sex" placeholder="请选择">
+              <el-option label="男" value="1" />
+              <el-option label="女" value="0" />
+            </el-select>
+          </el-form-item>
+        </el-col>
+        <el-col :span="12">
+          <el-form-item label="出生日期" prop="birth">
+            <el-date-picker
+              v-model="formUser.birth"
+              type="date"
+              placeholder="请输入"
+              style="width: 100%"
+            />
+          </el-form-item>
+        </el-col>
+      </el-row>
+      <el-row>
+        <el-form-item
+          label="地址"
+          prop="addr"
+        >
+          <el-input v-model="formUser.addr" placeholder="请输入地址" />
+        </el-form-item>
+      </el-row>
+      <el-row style="justify-content: flex-end">
+        <el-form-item>
+          <el-button type="primary" @click="handleCancel">取消</el-button>
+          <el-button type="primary" @click="onSubmit">确定</el-button>
+        </el-form-item>
+      </el-row>
+    </el-form>
+  </el-dialog>
 </template>
 
 <script setup>
@@ -45,6 +104,22 @@ const handleClick = () => {
 }
 
 const tableData = ref([])
+const action = ref('add')
+const dialogVisible = ref(true)
+const formUser = reactive({
+    sex: '1'
+})
+// 表单校验规则
+const rules = reactive({
+  name: [{ required: true, message: "姓名是必填项", trigger: "blur" }],
+  age: [
+    { required: true, message: "年龄是必填项", trigger: "blur" },
+    { type: "number", message: "年龄必须是数字" },
+  ],
+  sex: [{ required: true, message: "性别是必选项", trigger: "change" }],
+  birth: [{ required: true, message: "出生日期是必选项" }],
+  addr:[{ required: true, message: '地址是必填项' }]
+})
 
 const tableLabel = reactive([
     {
@@ -101,6 +176,15 @@ const handleDelete = (val) => {
         })
         getUserData()
     })
+}
+
+const handleClose = () => {
+    // 获取表单重置表单
+    dialogVisible.value = false
+}
+
+const handleCancel = () => {
+    dialogVisible.value = false
 }
 
 const {proxy} = getCurrentInstance()
